c5e8f4ec3a8e174a43e281dc462d97970563c91c
Ramon Ferreira <ramon_ferreira27@hotmail.com>
2019-04-10 11:48:54 -0300

    Criação de duas novas funções para organizar melhor o script.

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
-    int max = 1000, min = 1, guess = 500;
+    int max, min, guess;
 
     // Start is called before the first frame update
     void Start() {
+        StartGame();
+    }
+
+    void StartGame() {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
         Debug.Log("Bem-vindo ao Number Wizard!");
         Debug.Log("Escolha um número");
         Debug.Log("O maior número que você pode escolher é " + max);
         Debug.Log("O menor número que você pode escolher é " + min);
         Debug.Log("Diga-me: seu número foi maior ou menor que " + guess + "?");
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+        Debug.Log("Para cima = Maior, Para baixo = Menor, Aperte Enter = Correto");
         max++;
     }
 
@@ -20,16 +28,19 @@ public class NumberWizard : MonoBehaviour {
     void Update() {
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Então... seu número é " + guess + ", correto? ");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)) {          
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Então... seu número é " + guess + ", correto? ");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return)) {
             Debug.Log("Isso, eu já sabia que era esse número desde o início!!");
+            StartGame();
+        }
+        void NextGuess() {
+            guess = (max + min) / 2;
+            Debug.Log("Então... seu número é " + guess + ", correto? ");
         }
     }
 }
